7ca8025c18b656501c69eaba5fe857293ddbe391
John Eigenbrode <jeigenbrode@windstream.net>
2025-01-14 12:14:24 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by John Eigenbrode on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
